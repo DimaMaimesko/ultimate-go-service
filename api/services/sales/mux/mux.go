3bfe9b4dfa710234a20b2d00790b06b4798b05c8
dmytromaimesko <dima.maimesko@gmail.com>
2026-06-10 01:13:50 +0300

    add request logging middleware to sales service

--- api/services/sales/mux/mux.go
+++ api/services/sales/mux/mux.go
@@ -4,12 +4,14 @@ import (
 	"os"
 
 	"github.com/DimaMaimesko/ultimate-go-service/api/services/sales/route/sys/checkapi"
+	"github.com/DimaMaimesko/ultimate-go-service/app/api/mid"
+	"github.com/DimaMaimesko/ultimate-go-service/foundation/logger"
 	"github.com/DimaMaimesko/ultimate-go-service/foundation/web"
 )
 
 // WebAPI constructs a http.Handler with all application routes bound.
-func WebAPI(shutdown chan os.Signal) *web.App {
-	mux := web.NewApp(shutdown)
+func WebAPI(log *logger.Logger, shutdown chan os.Signal) *web.App {
+	mux := web.NewApp(shutdown, mid.Logger(log))
 
 	checkapi.Routes(mux)
 
